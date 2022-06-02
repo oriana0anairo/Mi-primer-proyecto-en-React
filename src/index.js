07892e11d2e0069b66a828d66bf96545ceb22397
Oriana Mendez <33820591+oriana0anairo@users.noreply.github.com>
2022-06-01 21:34:01 -0400

    Primer commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react"
+import ReactDOM from "react-dom"
+import Card from "./components/Card"
+import 'bootstrap/dist/css/bootstrap.css'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const container = document.getElementById('root')
+ReactDOM.render(<Card
+                    title="Techique Guides"
+                    description="Learn amazing street workout and calisthenics"
+                    img="https://firebasestorage.googleapis.com/v0/b/tutoriales-e4830.appspot.com/o/exercise.png?alt=media&token=b9c4b236-16a9-4a56-bba2-90c9660a0f06"
+                    leftColor="#A74CF2"
+                    rightColor="#617BFB"
+                />, container)
